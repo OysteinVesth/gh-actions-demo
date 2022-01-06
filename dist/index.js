c2fc884f22818b8b0c62adf239d9f8fc317b28c3
Øystein Lorentzen Vesth <oystein.vesth@gmail.com>
2022-01-06 20:45:43 +0100

    ci: console log context

--- dist/index.js
+++ dist/index.js
@@ -8452,7 +8452,12 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
+  github.context
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const contextpayload = JSON.stringify(github.context, undefined, 2)
+  console.log(`The context payload: ${contextpayload}`)
+  console.log('================================================')
+  console.log('================================================')
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
